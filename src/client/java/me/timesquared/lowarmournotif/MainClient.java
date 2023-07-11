d80c4b8db550306eb7d7367f2f25d6535a95ff6e
SummerNugget <summernugget1@gmail.com>
2023-07-11 20:46:41 +0300

    cleaned up code and fixed issue with mod not alerting helmet's durability

--- src/client/java/me/timesquared/lowarmournotif/MainClient.java
+++ src/client/java/me/timesquared/lowarmournotif/MainClient.java
@@ -15,6 +15,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 
 import java.io.IOException;
+import java.io.ObjectInputFilter;
 
 public class MainClient implements ClientModInitializer {
 	public static final MainClient INSTANCE = new MainClient();
@@ -44,6 +45,7 @@ public class MainClient implements ClientModInitializer {
 			mc.send(() -> mc.setScreen(new ConfigScreen(Text.of("config"))));
 			return 1;
 		})))); // Register config command
+		MainServer.LOGGER.info("Initialized mod successfully!");
 	}
 	
 	public void onTick() {
@@ -67,54 +69,37 @@ public class MainClient implements ClientModInitializer {
 			if (leggingsSlot.getItem() instanceof ArmorItem) leggingsDuraPercent = (double) leggingsCurrentDura / leggingsSlot.getMaxDamage();
 			if (bootsSlot.getItem() instanceof ArmorItem) bootsDuraPercent = (double) bootsCurrentDura / bootsSlot.getMaxDamage();
 			
-			if (helmetSlot.getItem() instanceof ArmorItem) {
-				if (!(lastHelmetTickPercent <= ((double) ConfigManager.getArmourPercentAlert() / 100)) && helmetDuraPercent < ((double) ((double) ConfigManager.getArmourPercentAlert() / 100) / 100)) {
-					if (ConfigManager.isSound()) mc.player.playSound(SoundEvents.ENTITY_IRON_GOLEM_HURT, SoundCategory.MASTER, 1f, 1f);
-					mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT,
-							Text.of("Armor Durability Low!"),
-							Text.of("Your helmet's durability is below " + ConfigManager.getArmourPercentAlert() + "%! Make sure to repair it soon")));
-				}
-			}
-			if (chestPlateSlot.getItem() instanceof ArmorItem) {
-				if (!(lastChestTickPercent <= ((double) ConfigManager.getArmourPercentAlert() / 100)) && chestDuraPercent < ((double) ConfigManager.getArmourPercentAlert() / 100)) {
-					if (ConfigManager.isSound()) mc.player.playSound(SoundEvents.ENTITY_IRON_GOLEM_HURT, SoundCategory.MASTER, 1f, 1f);
-					mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT,
-							Text.of("Armor Durability Low!"),
-							Text.of("Your chestplate's durability is below " + ConfigManager.getArmourPercentAlert() + "%! Make sure to repair it soon")));
-				}
-			}
-			if (leggingsSlot.getItem() instanceof ArmorItem) {
-				if (!(lastLeggingsTickPercent <= ((double) ConfigManager.getArmourPercentAlert() / 100)) && leggingsDuraPercent < ((double) ConfigManager.getArmourPercentAlert() / 100)) {
-					if (ConfigManager.isSound()) mc.player.playSound(SoundEvents.ENTITY_IRON_GOLEM_HURT, SoundCategory.MASTER, 1f, 1f);
-					mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT,
-							Text.of("Armor Durability Low!"),
-							Text.of("Your legging's durability is below " + ConfigManager.getArmourPercentAlert() + "%! Make sure to repair it soon")));
-				}
-			}
-			if (bootsSlot.getItem() instanceof ArmorItem) {
-				if (!(lastBootsTickPercent <= ((double) ConfigManager.getArmourPercentAlert() / 100)) && bootsDuraPercent < ((double) ConfigManager.getArmourPercentAlert() / 100)) {
-					if (ConfigManager.isSound()) mc.player.playSound(SoundEvents.ENTITY_IRON_GOLEM_HURT, SoundCategory.MASTER, 1f, 1f);
-					mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT,
-							Text.of("Armor Durability Low!"),
-							Text.of("Your boot's durability is below " + ConfigManager.getArmourPercentAlert() + "%! Make sure to repair it soon")));
-				}
-			}
+			checkDura(helmetSlot, helmetDuraPercent, lastHelmetTickPercent, "helmet");
+			checkDura(chestPlateSlot, chestDuraPercent, lastChestTickPercent, "chestplate");
+			checkDura(leggingsSlot, leggingsDuraPercent, lastLeggingsTickPercent, "leggings");
+			checkDura(bootsSlot, bootsDuraPercent, lastBootsTickPercent, "boots");
 			
-			/* MainServer.LOGGER.info(
-					"\nHelmet Dura: " + helmetCurrentDura +
-					"\nChest Dura: " + chestCurrentDura +
-					"\nLeggings Dura: " + leggingsCurrentDura +
-					"\nBoots Dura: " + bootsCurrentDura +
-					"\nPercent helm: " + helmetDuraPercent + " and " + lastHelmetTickPercent +
-					"\nPercent chest: " + chestDuraPercent + " and " + lastChestTickPercent +
-					"\nPercent leggings: " + leggingsDuraPercent + " and " + lastLeggingsTickPercent +
-					"\nPercent boots: " + bootsDuraPercent + " and " + lastBootsTickPercent
-			); */
+//			MainServer.LOGGER.info(
+//					"\nHelmet Dura: " + helmetCurrentDura +
+//					"\nChest Dura: " + chestCurrentDura +
+//					"\nLeggings Dura: " + leggingsCurrentDura +
+//					"\nBoots Dura: " + bootsCurrentDura +
+//					"\nPercent helm: " + helmetDuraPercent + " and " + lastHelmetTickPercent +
+//					"\nPercent chest: " + chestDuraPercent + " and " + lastChestTickPercent +
+//					"\nPercent leggings: " + leggingsDuraPercent + " and " + lastLeggingsTickPercent +
+//					"\nPercent boots: " + bootsDuraPercent + " and " + lastBootsTickPercent
+//			);
 			
 			lastHelmetTickPercent = helmetDuraPercent;
 			lastChestTickPercent = chestDuraPercent;
 			lastLeggingsTickPercent = leggingsDuraPercent;
 			lastBootsTickPercent = bootsDuraPercent;
+		}
+	}
+	
+	public void checkDura(ItemStack armourItem, double duraPercent, double lastTickPercent, String armourItemName) {
+		if (armourItem.getItem() instanceof ArmorItem) {
+			if (!(lastTickPercent <= ((double) ConfigManager.getArmourPercentAlert() / 100)) && duraPercent < ((double) ConfigManager.getArmourPercentAlert() / 100)) {
+				if (ConfigManager.isSound()) mc.player.playSound(SoundEvents.ENTITY_IRON_GOLEM_HURT, SoundCategory.MASTER, 1f, 1f);
+				mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT,
+						Text.of("Armor Durability Low!"),
+						Text.of("Your " + armourItemName + "'s durability is below " + ConfigManager.getArmourPercentAlert() + "%! Make sure to repair it soon")));
+			}
 		}
 	}
 }
